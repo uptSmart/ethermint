c76b94af60d854e07c1dcde3c6090308a55ec9fe
Dreamer <745124335@qq.com>
2023-06-02 15:39:46 +0800

    add extensions

---
+++ ethereum/eip712/extensions.go
@@ -0,0 +1,21 @@
+package eip712
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+type Options struct {
+	InterfaceRegistry codectypes.InterfaceRegistry
+	Amino             *codec.LegacyAmino
+}
+
+// SetOptions set the encoding config to the singleton codecs (Amino and Protobuf).
+// The process of unmarshaling SignDoc bytes into a SignDoc object requires having a codec
+// populated with all relevant message types. As a result, we must call this method on app
+// initialization with the app's encoding config.
+func SetOptions(opts Options) {
+	aminoCodec = opts.Amino
+	protoCodec = codec.NewProtoCodec(opts.InterfaceRegistry)
+}
